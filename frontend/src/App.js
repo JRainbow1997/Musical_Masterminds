83b2e1dbdb2af59ea102eb96c2342763ad30c0ab
Joshua Rainbow <jrainbow1997@gmail.com>
2020-12-07 16:48:58 +0000

    Initial file creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React from "react";
+import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
+import Navbar from "";
+import NavbarLoggedIn from "";
+import Logo from "./appImages/logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+          <Switch>
+            <Route exact path = "/main" component = {NavbarLoggedIn}/>
+            <Route exact path = "/" component = {Navbar}/>
+          </Switch>
+          </header>
+        </div>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
