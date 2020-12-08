bbe160c9ff44e4121a179253e86ef1fad4260487
Joshua Rainbow <jrainbow1997@gmail.com>
2020-12-08 17:18:39 +0000

    404 CSS

--- frontend/src/components/error/PageNotFound.js
+++ frontend/src/components/error/PageNotFound.js
@@ -4,11 +4,12 @@ import "./PageNotFound.css";
 
 function PageNotFound() {
     return(
-    <div class="errorWrapper">
+    <div class="error-wrapper">
         <h1>ERROR 404</h1>
         <h3>You've gone off track!</h3>
         <h4>Click below to get back where you need to be.</h4>
-        <a id="errorLink" href="/#"><Link to="/main">Bring me home!</Link></a>
+        <br/>
+        <a className="back" id="errorLink" href="/#"><Link to="/main">Bring me home!</Link></a>
     </div>
     );
 };
