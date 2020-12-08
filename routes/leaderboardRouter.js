97b090438509ce2a125b598af72d93060879f59c
Joe Bentley <joe.bentley.20@gmail.com>
2020-12-08 13:53:09 +0000

    Leaderboard Router

--- routes/leaderboardRouter.js
+++ routes/leaderboardRouter.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const LeaderBoard = require("../models/schemas/leaderboard");
+
 
 router.get("/", (req, res) => {
-    res.status(200).json({status : "leaderboard-worked"});
-    }
-);
+    LeaderBoard.find({}, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ status: "Not OK", err });
+        } else {
+            res.status(200).json({ status: "OK", result });
+        }
+    });
+});
+    
 
 module.exports = router;
