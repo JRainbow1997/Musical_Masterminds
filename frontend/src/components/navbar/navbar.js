d57c3429973f420b8b47a42fc452668baf2db6ab
Joshua Rainbow <jrainbow1997@gmail.com>
2020-12-08 13:53:27 +0000

    Adding frontend files

---
+++ frontend/src/components/navbar/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "./images/logo.png";
+import "./navBar.css";
+
+const Navbar = props => {
+    return (
+        <nav>
+            <div className="div-logo">
+                <img src={logo} className="logo" alt="logo" />
+            </div>
+            <div className = "div-links">
+                <ul className = "navbar-links">
+                    <li><Link to="/signin">Login</Link></li>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
