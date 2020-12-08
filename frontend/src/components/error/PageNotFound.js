d3507fc770d776ff62b1a51c1dd160802d0554a5
Joshua Rainbow <jrainbow1997@gmail.com>
2020-12-08 17:02:09 +0000

    Added CSS to navbar & signup/in pages

--- frontend/src/components/error/PageNotFound.js
+++ frontend/src/components/error/PageNotFound.js
@@ -6,11 +6,8 @@ function PageNotFound() {
     return(
     <div class="errorWrapper">
         <h1>ERROR 404</h1>
-        <h3>You've gone off track!
-            <br/>
-            <br/>
-        Click below to get back where you need to be.
-        </h3>
+        <h3>You've gone off track!</h3>
+        <h4>Click below to get back where you need to be.</h4>
         <a id="errorLink" href="/#"><Link to="/main">Bring me home!</Link></a>
     </div>
     );
