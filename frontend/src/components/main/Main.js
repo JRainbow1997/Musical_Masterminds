60c376169417e6fd0c898436839810284ec596d2
Joe Bentley <joe.bentley.20@gmail.com>
2020-12-10 11:06:17 +0000

    Signout Timer
    User is automatically signed out after 60 minutes

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import "./Main.css";
 
 function Main() {
-    return(
+
+  let history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push("/signout");
+    }, 3600000); //Signs user out after 5 minutes
+  });
+
+  return (
     <div class="mainWrapper">
-        <h1>Main Page</h1>
+      <h1>Main Page</h1>
     </div>
-    );
-};
+  );
+}
 
 export default Main;
