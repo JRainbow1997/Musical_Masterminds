61e9b63287d5e2da2fa98e407a9cad0b86d8ea92
Joshua Rainbow <jrainbow1997@gmail.com>
2020-12-10 15:47:57 +0000

    Adding questions to our quiz app

---
+++ frontend/src/components/quiz/LoadQuestions.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import React, { useState, } from "react";
+import { useHistory } from "react-router-dom"
+import "./Quiz.css";
+
+const LoadQuestions = () => {
+    let history = useHistory();
+    const [difficulty, setDifficulty] = useState("easy");
+    const [amount, setAmount] = useState("5");
+
+    const submitForm = (event) => {
+        let i = 0;
+        event.preventDefault()
+        axios
+            .get("http://localhost:5000/quiz", { params: { questions: amount, category: 13, difficulty: difficulty } })
+            .then((res) => {
+                let questions = [];
+                let type = [];
+                let answers = [];
+                while (i != amount){
+                    questions.push(res.data.results[i].question);
+                    type.push(res.data.results[i].type);
+                    answers.push(res.data.results[i].correct_answer);
+                    if (type[i] === "boolean"){
+                        answers.push(res.data.results[i].incorrect_answers[0]);
+                    }else if (type[i] === "multiple"){
+                        for (let j = 0; j<3; j++){
+                            answers.push(res.data.results[i].incorrect_answers[j]);
+                        }
+                    }
+                    i += 1;
+                }
+                sessionStorage.setItem("Questions", JSON.stringify(questions));
+                sessionStorage.setItem("Type", JSON.stringify(type));
+                sessionStorage.setItem("Answers", JSON.stringify(answers));
+                history.push("/Questions");
+            })
+            .catch((err) => {
+                alert((err, "No difficulty"))
+            })
+    }
+    const onChangeHandler = (event) => {
+        if (event.target.id === "difficulty") {
+            setDifficulty(event.target.value)
+        } else if (event.target.id === "amount") {
+            setAmount(event.target.value)
+        }
+    }
+
+    return (
+        <div class="quizWrapper">
+            <p>Select difficulity</p>
+            <form onSubmit={submitForm}>
+                <select name="difficulties" id="difficulty" onChange={onChangeHandler} value={difficulty}>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+                <select name="amount" id="amount" onChange={onChangeHandler} value={amount}>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="12">12</option>
+                </select>
+                <input type="submit" value="submit"></input>
+            </form>
+        </div>
+    );
+};
+
+export default LoadQuestions;
--- frontend/src/components/quiz/Quiz.js
+++ frontend/src/components/quiz/Quiz.js
@@ -1,54 +1,19 @@
-import axios from "axios";
-import React, { useState, } from "react";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import LoadQuestions from "./LoadQuestions";
+import Questions from "./Questions";
 import "./Quiz.css";
 
 const Quiz = () => {
 
-    const [difficulty, setDifficulty] = useState("easy");
-    const [amount, setAmount] = useState("5");
-
-
-    const submitForm = (event) => {
-        event.preventDefault()
-        axios
-            .get("http://localhost:5000/quiz", { params: { questions: amount, category: 13, difficulty: difficulty } })
-            .then((res) => {
-                console.log(res.data.results)
-            })
-            .catch((err) => {
-                alert((err, "No difficulty"))
-            })
-        console.log(difficulty)
-        console.log(amount)
-    }
-    const onChangeHandler = (event) => {
-        if (event.target.id === "difficulty") {
-            setDifficulty(event.target.value)
-        } else if (event.target.id === "amount") {
-            setAmount(event.target.value)
-        }
-    }
-
     return (
         <div class="quizWrapper">
-            <h1>Quiz Page</h1>
-            <p>Select difficulity</p>
-            <form onSubmit={submitForm}>
-                <select name="difficulties" id="difficulty" onChange={onChangeHandler} value={difficulty}>
-                    <option value="easy">Easy</option>
-                    <option value="medium">Medium</option>
-                    <option value="hard">Hard</option>
-                </select>
-                <select name="amount" id="amount" onChange={onChangeHandler} value={amount}>
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="12">12</option>
-                </select>
-                <input type="submit" value="submit"></input>
-            </form>
-
-            <h1>questions</h1>
-            
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/Quiz" component={LoadQuestions} /> 
+                    <Route exact path="/Questions" component={Questions} />  
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 };
