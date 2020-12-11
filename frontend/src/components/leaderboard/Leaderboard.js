2d2fff85579208069d3ed0105f019b8a1f2ff215
Madeline <madeline.harrop@gmail.com>
2020-12-11 15:20:09 +0000

    #58 Filtering leaderboard

--- frontend/src/components/leaderboard/Leaderboard.js
+++ frontend/src/components/leaderboard/Leaderboard.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from "react";
 import IdleTimerContainer from "../IdleTimerComponent/IdleTimerComponent";
 import "./Leaderboard.css";
 
+
 const Leaderboard = () => {
   const [leaderboard, setLeaderboard] = useState([]);
+  const [LBFilter, setLBFilter] = useState("all");
 
   const parseData = (data) => {
     const changedResults = data.map((item) => {
@@ -12,11 +14,14 @@ const Leaderboard = () => {
         return { "username": item.username, "score": res.score, "difficulty": res.difficulty, "date": new Date(res.date).toDateString() };
       });
     });
-    return [].concat.apply([], changedResults).sort((a, b) => { return b.score - a.score }).slice(0, 9);
+    const result = [].concat.apply([], changedResults).sort((a, b) => { return b.score - a.score }).slice(0, 9);
+    if (LBFilter != "all") {
+      return (result.filter((result) => { return (result.difficulty == LBFilter) }));
+    } 
+    return result
   };
 
   const getResponse = () => {
-    // let userId = sessionStorage.getItem("userId");
     axios
       .post("api/leaderboard")
       .then((res) => {
@@ -26,6 +31,14 @@ const Leaderboard = () => {
         alert(err, "No Leader");
       });
   };
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    getResponse()
+  }
+  const onChangeHandler = (event) => {
+    setLBFilter(event.target.value)
+  }
 
   useEffect(() => {
     if (leaderboard.length === 0) {
@@ -42,28 +55,42 @@ const Leaderboard = () => {
     <div className="leaderboardWrapper">
       <IdleTimerContainer />
       {(!sessionStorage.getItem('signedIn')) ? <div><h1 className="title">You are not signed in</h1></div> :
-        <table>
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>Score</th>
-              <th>Difficulty</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {leaderboard.map((playerResult, index) => {
-              return (
-                <tr>
-                  <td key={index}>{playerResult.username}</td>
-                  <td key={index}>{playerResult.score}</td>
-                  <td key={index}>{playerResult.difficulty}</td>
-                  <td key={index}>{playerResult.date}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <div>
+          <div>
+            <p>Which leaderboard would you like to view?</p>
+            <form onSubmit={onSubmit}>
+              <select name="difficulties" id="difficulty" onChange={onChangeHandler}>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+                <option value="all" selected>All</option>
+              </select>
+              <input type="submit" value="submit"></input>
+            </form>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>Score</th>
+                <th>Difficulty</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {leaderboard.map((playerResult, index) => {
+                return (
+                  <tr>
+                    <td key={index}>{playerResult.username}</td>
+                    <td key={index}>{playerResult.score}</td>
+                    <td key={index}>{playerResult.difficulty}</td>
+                    <td key={index}>{playerResult.date}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       }
     </div>
   );
