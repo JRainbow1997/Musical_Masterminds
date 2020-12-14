a033a7e8d2fbabd982c97c53dc294e7e9054ad02
Joe Bentley <joe.bentley.20@gmail.com>
2020-12-14 13:42:51 +0000

    Resetting User password
    #61 Frontend to Backend

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import IdleTimerContainer from "../IdleTimerComponent/IdleTimerComponent";
 import "./Profile.css";
 
@@ -6,19 +7,48 @@ function Profile() {
 
     document.title = "Profile | Musical Masterminds";
 
-    useEffect(() => {
-        if (!sessionStorage.getItem("signedIn")) {
-            return;
+    const [password, setPassword] = useState("");
+    const [passwordCheck, setPasswordCheck] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        axios.post("api/users/updatepassword", {
+            id: sessionStorage.getItem("userId"),
+            password: password,
+            passwordCheck: passwordCheck,
+        }).then((res) => {
+            if (res.data.status === "OK") {
+                alert("Password successfully changed")
+            }
+        }).catch((err) => {
+            alert("Error, unable to change passwords")
+        })
+    };
+
+    const onChangeHandler = (event) => {
+        if (event.target.id === "password") {
+            setPassword(event.target.value)
+        } else if (event.target.id === "passwordCheck") {
+            setPasswordCheck(event.target.value)
         }
-    })
+    };
+
+
     return (
         <div className="profileWrapper">
             <IdleTimerContainer />
             {(!sessionStorage.getItem('signedIn')) ? 
                 <div><h1 className="title">You are not signed in</h1></div> :
-                <div>
-                    <h2>{sessionStorage.getItem("username")}</h2>
-                    <input type="password" placeholder="Reset Password"></input>
+                <div className="password-reset">
+                    <form onSubmit={onSubmit}>
+                        <h2>{sessionStorage.getItem("username")}</h2>
+                        <label htmlFor="password">New password</label>
+                        <input id="password" name="password" type="password" value={password} placeholder="Reset Password" onChange={onChangeHandler}></input>
+                        <br />
+                        <label htmlFor="passwordCheck">Confirm password</label>
+                        <input id="passwordCheck" name="passwordCheck" value={passwordCheck} type="password" placeholder="Confirm Password" onChange={onChangeHandler}></input>
+                        <input value="submit" type="submit"></input>
+                    </form>
                 </div>  
             }
         </div>
